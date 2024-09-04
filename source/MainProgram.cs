9e0dde4a1efd790470669f88e4ebb3716302dc2b
codeonwort <codeonwort@gmail.com>
2024-09-04 22:11:35 +0900

    Create csharp project

---
+++ source/MainProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace UnSospiro
+{
+    internal class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

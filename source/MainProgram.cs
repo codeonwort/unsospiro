8aa0077392fcb94f0991651f08093857d4a9e9a8
codeonwort <codeonwort@gmail.com>
2024-09-05 23:07:24 +0900

    Add dummy REPL

--- source/MainProgram.cs
+++ source/MainProgram.cs
@@ -2,9 +2,44 @@
 {
     internal class MainProgram
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: unsospiro [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string file)
+        {
+            string content = File.ReadAllText(Path.GetFullPath(file), System.Text.Encoding.Default);
+            Run(content);
+        }
+
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                string? line = Console.ReadLine();
+                if (line == null) break;
+                Run(line);
+            }
+        }
+
+        private static void Run(string content)
+        {
+            // TODO: Implement Scanner, scan tokens, and print the tokens.
+            Console.WriteLine($"input: {content}");
         }
     }
 }

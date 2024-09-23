c79724c07a297bc2c85ab3267c1e390d287d91d2
codeonwort <codeonwort@gmail.com>
2024-09-24 00:34:37 +0900

    Implement top-down parser

--- source/MainProgram.cs
+++ source/MainProgram.cs
@@ -61,16 +61,30 @@
         {
             Scanner scanner = new Scanner(source);
             List<Token> tokens = scanner.ScanTokens();
+            Parser parser = new Parser(tokens);
+            Expr expression = parser.Parse();
 
-            foreach (var token in tokens)
-            {
-                Console.WriteLine($"{token}");
-            }
+            if (hadError) return;
+
+            Console.WriteLine(new AstPrinter().Print(expression));
+            //foreach (var token in tokens) Console.WriteLine($"{token}");
         }
 
         internal static void Error(int line, string message)
         {
             Report(line, "", message);
+        }
+
+        internal static void Error(Token token, string message)
+        {
+            if (token.type == TokenType.EOF)
+            {
+                Report(token.line, " at end", message);
+            }
+            else
+            {
+                Report(token.line, $" at '{token.lexeme}'", message);
+            }
         }
 
         private static void Report(int line, string where, string message)

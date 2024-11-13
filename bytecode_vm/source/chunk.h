8463243e1367c9e6180aa5e7af8daa3405279035
codeonwort <codeonwort@gmail.com>
2024-11-13 19:43:03 +0900

    Implement memory alloc utils

--- bytecode_vm/source/chunk.h
+++ bytecode_vm/source/chunk.h
@@ -12,3 +12,7 @@ typedef struct {
 	int capacity;
 	uint8_t* code;
 } Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
---
+++ bytecode_vm/source/memory.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "common.h"
+
+// TODO: Initial size = 8 and growth multiplier = 2. Profile and find optimal parameters.
+#define GROW_CAPACITY(capacity) \
+	((capacity) < 8 ? 8 : (capacity * 2))
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+	(type*)reallocate(pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount))
+
+#define FREE_ARRAY(type, pointer, oldCount) \
+	reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+// This function will be globally used for dynamic memory management.
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);

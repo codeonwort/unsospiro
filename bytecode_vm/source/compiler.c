15f3459f0ab46b3beaa35ed2a6a46ed105b4b00e
codeonwort <codeonwort@gmail.com>
2024-11-25 18:27:37 +0900

    Stub code for scanner loop.
    TODO: Actually scan tokens.

--- bytecode_vm/source/compiler.c
+++ bytecode_vm/source/compiler.c
@@ -6,4 +6,18 @@
 
 void compile(const char* source) {
 	initScanner(source);
+	int line = -1;
+	for (;;) {
+		Token token = scanToken();
+		if (token.line != line) {
+			printf("%4d", token.line);
+			line = token.line;
+		} else {
+			printf("  | ");
+		}
+		// %.*s maps to first (token.length) characters at (token.start)
+		printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+		if (token.type == TOKEN_EOF) break;
+	}
 }

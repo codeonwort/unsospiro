13568af4b992c901d4c2adba59d95e4c0d3ac402
codeonwort <codeonwort@gmail.com>
2024-12-11 18:17:29 +0900

    hash table

---
+++ bytecode_vm/source/table.c
@@ -0,0 +1,120 @@
+#include "table.h"
+#include "memory.h"
+#include "object.h"
+#include "value.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+// #todo: Optimal load factor
+#define TABLE_MAX_LOAD 0.75
+
+void initTable(Table* table) {
+	table->count = 0;
+	table->capacity = 0;
+	table->entries = NULL;
+}
+
+void freeTable(Table* table) {
+	FREE_ARRAY(Entry, table->entries, table->capacity);
+	initTable(table);
+}
+
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+	uint32_t index = key->hash % capacity;
+	Entry* tombstone = NULL;
+
+	// NOTE: adjustCapacity() guarantees no infinite loop.
+	for (;;) {
+		Entry* entry = &entries[index];
+		if (entry->key == NULL) {
+			if (IS_NIL(entry->value)) {
+				return tombstone != NULL ? tombstone : entry;
+			} else {
+				if (tombstone == NULL) tombstone = entry;
+			}
+		} else if (entry->key == key) {
+			return entry;
+		}
+
+		// Linear probing
+		index = (index + 1) % capacity;
+	}
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+	if (table->count == 0) return false;
+
+	Entry* entry = findEntry(table->entries, table->capacity, key);
+	if (entry->key == NULL) return false;
+
+	*value = entry->value;
+	return true;
+}
+
+static void adjustCapacity(Table* table, int capacity) {
+	Entry* entries = ALLOCATE(Entry, capacity);
+	for (int i = 0; i < capacity; ++i) {
+		entries[i].key = NULL;
+		entries[i].value = NIL_VAL;
+	}
+
+	// We don't need to copy tombstones, so zero it and only calculate entries.
+	table->count = 0;
+
+	for (int i = 0; i < table->capacity; ++i) {
+		Entry* entry = &table->entries[i];
+		if (entry->key == NULL) continue;
+
+		Entry* dest = findEntry(entries, capacity, entry->key);
+		dest->key = entry->key;
+		dest->value = entry->value;
+		table->count++; // Increase only if real entry.
+	}
+
+	FREE_ARRAY(Entry, table->entries, table->capacity);
+	table->entries = entries;
+	table->capacity = capacity;
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+	if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+		int capacity = GROW_CAPACITY(table->capacity);
+		adjustCapacity(table, capacity);
+	}
+
+	Entry* entry = findEntry(table->entries, table->capacity, key);
+	bool isNewKey = entry->key == NULL;
+	// Do not increase count if a tombstone is being filled.
+	if (isNewKey && IS_NIL(entry->value)) table->count++;
+
+	entry->key = key;
+	entry->value = value;
+	return isNewKey;
+}
+
+bool tableDelete(Table* table, ObjString* key) {
+	// Tombstone trick: Don't actually delete the entry, just replace it with a special marker.
+	// When performing probing, if an entry is actually deleted, the probe sequence is broken.
+
+	if (table->count == 0) return false;
+
+	// Find the entry.
+	Entry* entry = findEntry(table->entries, table->capacity, key);
+	if (entry->key == NULL) return false;
+
+	// Put a tombstone to the entry.
+	// NOTE: Tombstone still contributes to table->count, so does not decrease it.
+	entry->key = NULL;
+	entry->value = BOOL_VAL(true);
+	return true;
+}
+
+void tableAddAll(Table* from, Table* to) {
+	for (int i = 0; i < from->capacity; ++i) {
+		Entry* entry = &from->entries[i];
+		if (entry->key != NULL) {
+			tableSet(to, entry->key, entry->value);
+		}
+	}
+}
---
+++ bytecode_vm/source/table.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+	ObjString* key;
+	Value value;
+} Entry;
+
+typedef struct {
+	int count;    // Entry count + Tombstone count.
+	int capacity; // Max size. (load Factor = count / capacity)
+	Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableSet(Table* table, ObjString* key, Value value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table* from, Table* to);

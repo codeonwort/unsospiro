e84398e31cbd8d5bd543ae9492b63904a304875a
codeonwort <codeonwort@gmail.com>
2025-01-04 17:11:14 +0900

    Disable debug info

--- bytecode_vm/source/common.h
+++ bytecode_vm/source/common.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define DEBUG_PRINT_CODE      1
-#define DEBUG_TRACE_EXECUTION 1
+#define DEBUG_PRINT_CODE      0
+#define DEBUG_TRACE_EXECUTION 0
 
 #define UINT8_COUNT           (UINT8_MAX + 1)
